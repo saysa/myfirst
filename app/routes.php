c8cde2522458e4475b7d8fb0ab9c8a604f159b2b
Saysa <saysabou@gmail.com>
2013-09-05 12:17:10 +0200

    first simple view that extends a layout

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,7 @@ Route::get('/', function()
 
 Route::get('users', function()
 {
-	return 'Users!';
+	return View::make('users');
 });
 
 /*
---
+++ app/views/layout.blade.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>Laravel layout</h1>
+		
+		@yield('content')
+	</body>
+</html>
